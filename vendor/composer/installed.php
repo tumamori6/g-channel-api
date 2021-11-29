f20a96ab2b854dcb97e42adb22631cab55199b98
tumamori6 <tumamori6@gmail.com>
2021-11-30 08:55:47 +0900

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7fa614f94665e76c199630e16155ad25c68d24ff',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7fa614f94665e76c199630e16155ad25c68d24ff',
+            'dev_requirement' => false,
+        ),
+    ),
+);
