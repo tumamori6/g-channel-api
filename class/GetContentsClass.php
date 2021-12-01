ba729bb4a156889c385090a44e8ee2a602c0188c
tumamori6 <tumamori6@gmail.com>
2021-12-01 12:20:59 +0900

    　

--- class/GetContentsClass.php
+++ class/GetContentsClass.php
@@ -8,11 +8,21 @@ class GetContentsClass
 		require(dirname(__FILE__) . '/../vendor/autoload.php');
 	}
 
-	public function htmlConvertObject($url)
+	private function htmlConvertObject($url)
 	{
 		$html = file_get_contents($url);
 		$obj  = phpQuery::newDocument($html);
 		return $obj;
+	}
+
+	private function checkCategory($title)
+	{
+		for ($i = 0; $i < count(CATEGORY_LIST); $i++) {
+			if (strpos($title, CATEGORY_LIST[$i])) {
+				$category = CATEGORY_LIST[$i];
+			}
+		}
+		return $category;
 	}
 
 	public function getArticleList($url)
@@ -22,10 +32,12 @@ class GetContentsClass
 
 		foreach ($articles as $article) {
 			$article_title = utf8_decode(pq($article)->find('.title')->text());
-			$article_url   = 'https://girlschannel.net'.pq($article)->attr("href");
+			$article_url   = 'https://girlschannel.net' . pq($article)->attr("href");
+			$category      = $this->checkCategory(utf8_decode(pq($article)->find('.title')->text()));
 			$datas[] = [
 				'article_title' => $article_title,
 				'article_url'   => $article_url,
+				'category'      => $category,
 			];
 		}
 
