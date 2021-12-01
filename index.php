049f3ce95a3f63c15be6b764934e3e3061345e41
tumamori6 <tumamori6@gmail.com>
2021-12-01 12:06:10 +0900

    カテゴリチェック追加

--- index.php
+++ index.php
@@ -3,10 +3,8 @@ require('class/ModelClass.php');
 require('class/GetContentsClass.php');
 $model = new ModelClass();
 $contents = new GetContentsClass();
-//$data  = $model->getAll('tests');
-//$data = $contents->htmlConvertObject(URL_LIST[CATEGORY]);
+
 $data = $contents->getArticleList(CONTENTS_URL);
-//$data = file_get_contents(CONTENTS_URL);
 
 include('index.html');
 ?>
